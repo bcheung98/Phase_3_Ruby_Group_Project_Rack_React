725e06e92799cd345a431e99410fb08f15bd1a39
Brian <briantcheung@gmail.com>
2021-04-19 16:22:26 -0700

    Added basic rack requests

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -5,7 +5,27 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+      return [200, { "Content-Type" => "application/json" }, [ {:message => "test response!"}.to_json ]]
+
+    elsif req.path.match(/my_courses/) && req.get?
+      return [200, { "Content-Type" => "application/json" }, [ Student.first.courses.map {|c| {
+        id: c.id, 
+        subject: c.subject, 
+        number: c.number, 
+        title: c.title, 
+        time: c.time,
+        teacher: c.teacher.name
+      }}.to_json ]]
+
+    elsif req.path.match(/courses/) && req.get?
+      return [200, { "Content-Type" => "application/json" }, [ Course.all.map {|c| {
+        id: c.id, 
+        subject: c.subject, 
+        number: c.number, 
+        title: c.title, 
+        time: c.time,
+        teacher: c.teacher.name
+      }}.to_json ]]
 
     else
       resp.write "Path Not Found"
