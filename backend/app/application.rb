9d0c55b557571135d698a926f71c6993796b52be
Brian <briantcheung@gmail.com>
2021-04-22 11:41:24 -0700

    Added user registration validation

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -7,6 +7,22 @@ class Application
     if req.path.match(/test/) 
       return [200, { "Content-Type" => "application/json" }, [ {:message => "test response!"}.to_json ]]
 
+    elsif req.path.match(/users/) && req.post?
+      data = JSON.parse req.body.read
+      student_usernames = Student.all.map {|s| s.username}
+      student_emails = Student.all.map {|s| s.email}
+      if !student_usernames.include?(data["username"]) 
+        if !student_emails.include?(data["email"])
+          Student.create(data)
+          msg = "Successfully registered!"
+        else
+          msg = "ERROR: That email has already been registered"
+        end
+      else
+        msg = "ERROR: That username has already been registered"
+      end
+      return [200, { "Content-Type" => "application/json" }, [ data.to_json ]] 
+
     elsif req.path.match(/my_courses/) && req.get?
       return [200, { "Content-Type" => "application/json" }, [ Student.first.courses.map {|c| {
         id: c.id, 
